41aeb0bb36cb0b100e27d927da4b30c9e7ef1932
modeverv <modeverv@gmail.com>
2017-04-02 02:04:40 +0900

    initial

---
+++ Assets/CarController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+	// 速度
+	float speed = 0;
+	// 初期場所
+	Vector2 startPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// スワイプの長さを求める
+		detectMouse ();
+		// 移動
+		move ();
+		// 減衰
+		decay ();
+	}
+
+	/**
+	 * スワイプの長さを求める
+	 */
+	void detectMouse ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			this.startPos = Input.mousePosition;
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			Vector2 endPos = Input.mousePosition;
+			float swipeLength = endPos.x - this.startPos.x;
+
+			this.speed = swipeLength / 500.0f;
+			play ();
+		}
+			
+	}
+
+	/**
+	 * 移動
+	 */ 
+	void move ()
+	{
+		transform.Translate (this.speed, 0, 0);
+	}
+
+	/**
+	 * 減衰
+	 */ 
+	void decay ()
+	{
+		this.speed *= 0.98f;
+	}
+
+	/**
+	 * 音声再生
+	 */ 
+	void play ()
+	{
+		GetComponent<AudioSource> ().Play ();
+	}
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameDirector : MonoBehaviour
+{
+
+	GameObject car;
+	GameObject flag;
+	GameObject distance;
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.car = GameObject.Find ("car");
+		this.flag = GameObject.Find ("flag");
+		this.distance = GameObject.Find ("Distance");
+		this.distance.GetComponent<Text> ().text =
+			@"スワイプしてスタート！\nフラッグまでできるだけ近く！";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float length = getLength ();
+		if (length >= 0f) {
+			setText ("ゴールまで" + length.ToString ("F2") + "m");
+		} else {
+			setText ("ゲームオーバー");
+		}
+
+	}
+
+	float getLength ()
+	{
+		return this.flag.transform.position.x - this.car.transform.position.x;
+	}
+
+	void setText (String str)
+	{
+		this.distance.GetComponent<Text> ().text = str;
+	}
+}
